3b0328889952694e0fa4bdf61e625f1614d00733
ShmulikThePro <galgaling2011@gmail.com>
2024-03-16 09:24:18 +0200

    small bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/quizlet-clone",
-  output: "export",
   reactStrictMode: true,
 };
 
